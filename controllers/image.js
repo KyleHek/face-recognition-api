587da6994ac779dc4c103afa9da7a00a2735a65f
kylehek <kylehek99@gmail.com>
2023-11-13 11:33:36 +0000

    updated image import

--- controllers/image.js
+++ controllers/image.js
@@ -63,11 +63,6 @@ const handleImage = (req, res, db) => {
         .catch(err => res.status(400).json('unable to get entries'));
 }
 
-module.exports = {
-    handleImage: handleImage,
-    handleAPICall: handleAPICall
-}
-
 export {
     handleImage,
     handleAPICall
